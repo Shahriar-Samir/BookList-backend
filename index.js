6c3a0f1be7020eaa88a1bb16626b6602957dcc21
Shahriar Samir <138759491+Shahriar-Samir@users.noreply.github.com>
2023-10-10 00:40:30 +0600

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ const app = express()
 const  port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(cors())
+app.use(cors(
+    {
+        origin:"https://booklist-frontend-iota.vercel.app/"
+    }
+))
 // config
 
 
